ce503aa00f2eeb15770837e826953c7f837f693f
HansRubenecia <112526065+HansRubenecia@users.noreply.github.com>
2023-10-25 16:52:14 +0800

    Update Main.java

--- theoryPl(Laboratory5)/src/Main.java
+++ theoryPl(Laboratory5)/src/Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,30 +6,57 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        // Check for the presence of a semicolon at the end
-        if (input.matches(".+;\\s*$")) {
-            // Split the input by semicolons to separate statements
-            String[] statements = input.split(";");
+        // Split the input by semicolons to separate statements
+        String[] statements = input.split(";");
 
-            for (String statement : statements) {
-                // Remove leading and trailing whitespace
-                statement = statement.trim();
+        for (String statement : statements) {
+            // Split each statement into tokens by whitespace
+            String[] tokens = statement.trim().split("\\s+");
 
-                if (isValidDeclaration(statement)) {
+            if (tokens.length >= 4 && tokens[2].equals("=")) {
+                String dataType = tokens[0];
+                String variableName = tokens[1];
+                String assignmentOperator = tokens[2];
+                String value = tokens[3];
+
+                if (isValidAssignment(dataType, variableName, value)) {
                     System.out.println("Semantically Correct: " + statement);
                 } else {
                     System.out.println("Semantically Incorrect: " + statement);
                 }
+            } else {
+                System.out.println("Invalid statement: " + statement);
             }
-        } else {
-            System.out.println("Invalid statement: Missing or misplaced semicolon");
         }
     }
 
-    // Perform a basic semantic check for variable declaration
-    public static boolean isValidDeclaration(String statement) {
-        // Use a regular expression to match valid variable declarations like "int x;"
-        String pattern = "^(int|String|float|char|long|byte|boolean|double)\\s+[a-zA-Z_][a-zA-Z0-9_]*;$";
-        return Pattern.matches(pattern, statement);
+    // Perform a more comprehensive semantic check
+    public static boolean isValidAssignment(String dataType, String variableName, String value) {
+        if (variableName.matches("[a-zA-Z_][a-zA-Z0-9_]*")) {
+            try {
+                if (dataType.equals("int")) {
+                    int intValue = Integer.parseInt(value);
+                    return true;
+                } else if (dataType.equals("double")) {
+                    double doubleValue = Double.parseDouble(value);
+                    return true;
+                } else if (dataType.equals("float")) {
+                    float floatValue = Float.parseFloat(value);
+                    return true;
+                } else if (dataType.equals("char") && value.matches("'.'")) {
+                    char charValue = value.charAt(1);
+                    return true;
+                } else if (dataType.equals("boolean")) {
+                    if (value.equals("true") || value.equals("false")) {
+                        return true;
+                    }
+                }
+                // Handle other data types and checks here
+            } catch (NumberFormatException e) {
+                // If parsing as the specified data type fails, it's an invalid assignment
+                return false;
+            }
+        }
+        return false;
     }
 }
